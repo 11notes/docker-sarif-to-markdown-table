4a7f0df28ab643c245157843ef845a236061f6f9
ElevenNotes <github@11notes.ch>
2025-02-13 13:22:26 +0100

    debug

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/ash
-  if [ -z "${1}" ]; then
-    echo $@
-    echo "${1}" | sarif-to-markdown-table > ${APP_ROOT}/sarif.md
-    cat ${APP_ROOT}/sarif.md
-    echo "markdown=$(cat ${APP_ROOT}/sarif.md)" >> $GITHUB_OUTPUT
-  else
-    exec "$@"
-  fi
+
+  echo $@
