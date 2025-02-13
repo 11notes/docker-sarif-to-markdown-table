d5f9498966194283dd4beebd8482d534e7ddb913
ElevenNotes <github@11notes.ch>
2025-02-13 15:45:02 +0100

    debug v6

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
   if [ -f ${1} ]; then
+    echo "converting ${1} to markdown"
     cat ${1} | sarif-to-markdown-table > ${APP_ROOT}/sarif.md
     echo "markdown=$(cat ${APP_ROOT}/sarif.md)" >> $GITHUB_OUTPUT
   fi
