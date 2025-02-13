cc0e41105569edbe2881f9dbed2c20bdcbf551dc
ElevenNotes <github@11notes.ch>
2025-02-13 15:19:30 +0100

    correct github home action

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-  printf '%d args:' "$#"
-  printf " '%s'" "$@"
-  printf '\n'
-  
-  ls -lah /github/*
-
-  echo -e "\n\n# TEST" >> /github/workspace/README.md
+#!/bin/ash
+  SARIF=/github/home/${1}
+  SARIF_MD=/github/home/sarif.md
+  if [ -f ${SARIF} ]; then
+    cat ${SARIF} | sarif-to-markdown-table > ${SARIF_MD}
+    cat ${SARIF_MD}
+    echo "markdown=$(cat ${SARIF_MD})" >> $GITHUB_OUTPUT
+  fi
