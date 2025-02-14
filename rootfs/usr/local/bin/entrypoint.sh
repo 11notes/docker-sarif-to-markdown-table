5199941cd6c474d473f593a0c072b8f4d35b1bf2
ElevenNotes <github@11notes.ch>
2025-02-14 08:44:59 +0100

    debug v7

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/ash
-  echo "file path is ${1}"
+  echo "checking if report ${1} exists"
   if [ -f ${1} ]; then
     echo "converting ${1} to markdown"
     cat ${1} | sarif-to-markdown-table > ${APP_ROOT}/sarif.md
     echo "markdown=$(cat ${APP_ROOT}/sarif.md)" >> $GITHUB_OUTPUT
+    cat ${APP_ROOT}/sarif.md
   fi
