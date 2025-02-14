58872b8afc27b6d338f736cc7c464a39d37c65d0
ElevenNotes <github@11notes.ch>
2025-02-14 09:00:19 +0100

    debug v8

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/ash
-  echo "checking if report ${1} exists"
+  GITHUB_WORKFLOW_HOME=/github/home
   if [ -f ${1} ]; then
-    echo "converting ${1} to markdown"
-    cat ${1} | sarif-to-markdown-table > ${APP_ROOT}/sarif.md
-    echo "markdown=$(cat ${APP_ROOT}/sarif.md)" >> $GITHUB_OUTPUT
-    cat ${APP_ROOT}/sarif.md
+    cat ${1} | sarif-to-markdown-table > ${GITHUB_WORKFLOW_HOME}/sarif.md
+    cat ${GITHUB_WORKFLOW_HOME}/sarif.md
+
+    # multiline GITHUB_OUTPUT
+    EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+    echo "markdown<<$EOF" >> $GITHUB_OUTPUT
+    echo "$(cat ${GITHUB_WORKFLOW_HOME}/sarif.md)" >> $GITHUB_OUTPUT
+    echo "$EOF" >> $GITHUB_OUTPUT
+  else
+    echo "sarif report ${1} does not exist or path is wrong. Use ${GITHUB_WORKFLOW_HOME} within containers"
+    ls -lah ${GITHUB_WORKFLOW_HOME}/*
   fi
